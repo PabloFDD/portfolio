81789594a4051e3912d0df61d5cebbe58dc21a4d
PabloFDD <pablofdelboni@gmail.com>
2022-08-05 16:58:28 -0300

    adicionado um efeito para a seção de welcome e um botão para voltar para o topo da página

---
+++ js/main.js
@@ -0,0 +1,53 @@
+consoleText(['Olá, seja bem vindo ao meu portfólio!', 'Criado por Pablo Delboni.'], 'text',['lightblue']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+}
+
+window.addEventListener('scroll', function(){
+  let scroll = document.querySelector('.button-top');
+  scroll.classList.toggle('active', window.scrollY> 600);
+});
+
+var button = document.querySelector('.button-top')
+button.addEventListener('click', function(){
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+ 
